5c42f6aee3eaf67a5885d602893fa522c0f54341
mwikya <mwikya3@gmail.com>
2019-07-07 13:06:08 +0300

    enabled public-read for uploaded files

--- cli_tools.py
+++ cli_tools.py
@@ -1,21 +1,21 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-	prog='digital-ocean-ss3-client',
-	description="Command line script to upload files to digital ocean spaces"
-	)
+    prog='digital-ocean-ss3-client',
+    description="Command line script to upload files to digital ocean spaces"
+)
 
 parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
 
 parser.add_argument(
-	'-d','--dir',
-	help="the root directory to upload.",
-	dest='dir',
-	required=True
+    '-d', '--dir',
+    help="the root directory to upload.",
+    dest='dir',
+    required=True
 )
 
 parser.add_argument(
-	'-b','--bucket',
-	help="default='backup', if left blank ",
-	dest='bucket'
-	)
+    '-b', '--bucket',
+    help="default='backup', if left blank ",
+    dest='bucket'
+)
--- spaces.py
+++ spaces.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import json
 import time
 import logging
 from colors import color
@@ -9,85 +8,90 @@ from boto3 import session
 
 from cli_tools import parser
 from utils import ProgressPercentage
-from config import (ACCESS_ID,SECRET_KEY,SERVICE_NAME,REGION_NAME,ENDPOINT_URL,BUCKET_NAME)
+from config import (ACCESS_ID, SECRET_KEY, SERVICE_NAME, REGION_NAME, ENDPOINT_URL, BUCKET_NAME)
 
 cwd_dir = os.getcwd()
 logs_dir = os.path.join(cwd_dir, '.logs')
 failed_dir = os.path.join(cwd_dir, '.failed_uploads')
 if not os.path.exists(logs_dir):
     os.makedirs(logs_dir)
-logging.basicConfig(filename=os.path.join(logs_dir,'logs.log'), 
-		filemode='a',format='%(asctime)s %(levelname)s %(message)s',
-	)
+logging.basicConfig(filename=os.path.join(logs_dir, 'logs.log'),
+                    filemode='a', format='%(asctime)s %(levelname)s %(message)s',
+                    )
 if not os.path.exists(failed_dir):
-	os.makedirs(failed_dir)
+    os.makedirs(failed_dir)
 
 
 def log_error(error_file):
-	log_file = os.path.join(failed_dir,"%s.txt"%time.strftime("%Y%m%d"))
-	if not os.path.exists(log_file):
-		with open(log_file, 'w'):
-			pass	
+    log_file = os.path.join(failed_dir, "%s.txt" % time.strftime("%Y%m%d"))
+    if not os.path.exists(log_file):
+        with open(log_file, 'w'):
+            pass
 
-	with open(log_file, 'a') as f:
-		f.write(error_file)
+    with open(log_file, 'a') as f:
+        f.write(error_file)
+
 
 # Initiate session
 session = session.Session()
 
+
 def set_client():
-	client = session.client(
-		service_name=SERVICE_NAME,
-		region_name=REGION_NAME,
-		endpoint_url=ENDPOINT_URL,
-		aws_access_key_id=ACCESS_ID,
-		aws_secret_access_key=SECRET_KEY
-	)
-	return client
+    client = session.client(
+        service_name=SERVICE_NAME,
+        region_name=REGION_NAME,
+        endpoint_url=ENDPOINT_URL,
+        aws_access_key_id=ACCESS_ID,
+        aws_secret_access_key=SECRET_KEY
+    )
+    return client
 
-def upload_dir(dirpath,bucket_name=None):
-	client = set_client()
-	if not os.path.isdir(dirpath):
-		raise Exception("Not a Valid Directory :%s" % (dirpath))
-	bucket = BUCKET_NAME
-	if bucket_name:
-		bucket = bucket_name
 
-	file_count = 0
-	error_count = 0
+def upload_dir(dirpath, bucket_name=None):
+    client = set_client()
+    if not os.path.isdir(dirpath):
+        raise Exception("Not a Valid Directory :%s" % (dirpath))
+    bucket = BUCKET_NAME
+    if bucket_name:
+        bucket = bucket_name
 
-	print(color("Initializing ...\n",fg="yellow"))
-	for (root, dirs, filenames) in os.walk(top=dirpath, topdown=True):
-		for filename in filenames:
-			filepath = os.path.join(root, filename)
-			upload_path = os.path.relpath(filepath,start="personal_docs").lstrip("../")
-			try:
-				print(color("%s \nuploading...\n"%filepath,fg='blue'))
-				client.upload_file(
-						filepath,
-						bucket, 
-						upload_path,
-						Callback=ProgressPercentage(filepath)
-					)
-				print(color("Success\n",fg="lime"))
+    file_count = 0
+    error_count = 0
 
-				file_count += 1
-			except Exception as e:
-				logging.error('FilePath: %s' % filepath, exc_info=True)
-				error_count += 1
-				print(color("Error encountered: %s"%e,fg="#ff1a1a"))
-				log_error("%s\n"%filepath)
-				continue
-	
-	print(color("Total number of files uploaded: %d.\n" % file_count,fg="lime"))	
-	print(color("Total number of errors encountered: %d." % error_count,fg="#ff1a1a"))
-	if error_count > 1:
-		print(color("Please Check the log file located at .logs in the current working directory",fg='#ff1a1a'))
+    print(color("Initializing ...\n", fg="yellow"))
+    for (root, dirs, filenames) in os.walk(top=dirpath, topdown=True):
+        for filename in filenames:
+            filepath = os.path.join(root, filename)
+            upload_path = os.path.relpath(filepath, start="personal_docs").lstrip("../")
+            try:
+                print(color("%s \nuploading...\n" % filepath, fg='blue'))
+                client.upload_file(
+                    filepath,
+                    bucket,
+                    upload_path,
+                    ExtraArgs={'ACL': 'public-read'},
+                    Callback=ProgressPercentage(filepath)
+                )
+                print(color("Success\n", fg="lime"))
+
+                file_count += 1
+            except Exception as e:
+                logging.error('FilePath: %s' % filepath, exc_info=True)
+                error_count += 1
+                print(color("Error encountered: %s" % e, fg="#ff1a1a"))
+                log_error("%s\n" % filepath)
+                continue
+
+    print(color("Total number of files uploaded: %d.\n" % file_count, fg="lime"))
+    if error_count > 0:
+        print(color("Total number of errors encountered: %d." % error_count, fg="#ff1a1a"))
+        print(color("Please Check the log file located at .logs in the current working directory", fg='#ff1a1a'))
+
 
 if __name__ == '__main__':
-	args = parser.parse_args()
-	root_dir = args.dir.rstrip("/")
-	bucket_name = args.bucket
-	upload_dir(root_dir,bucket_name)
+    args = parser.parse_args()
+    root_dir = args.dir.rstrip("/")
+    bucket_name = args.bucket
+    upload_dir(root_dir, bucket_name)
 
 # /home/coder/Documents/personal_docs/
